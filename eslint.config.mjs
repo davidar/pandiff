7347d7885099140f12951f23faa529160d6a2f17
David A Roberts <d@vidr.cc>
2025-03-22 12:01:37 +1000

    Upgrade deps

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    js.configs.recommended,
+    ...compat.extends("./node_modules/gts/"),
+    {
+        ignores: ["**/lib/**"]
+    }
+];
